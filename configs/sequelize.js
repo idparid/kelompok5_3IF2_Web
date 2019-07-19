c1f19bb2b2eb2388fbd8c214622cea1040bc4717
idparid <161014008paridm@gmail.com>
2019-07-19 19:54:20 +0700

    update squelize

--- configs/sequelize.js
+++ configs/sequelize.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('bookstore', 'root', '', {
+const sequelize = new Sequelize('bookstore', 'root', 'password', {
 	host: 'localhost',
 	dialect: 'mysql'
 });
